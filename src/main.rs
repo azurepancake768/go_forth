079d786d42743d0e2855855bbe63fc48dd550284
Vavřinec Kubíček <vavrinec.kubicek@gmail.com>
2024-04-11 15:46:55 +0200

    Refactor tile types to be structs implementing the Tile trait
    some more general refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,184 +1,70 @@
-use bevy::{prelude::*, window::PrimaryWindow};
+mod bevy_backend;
+use bevy::prelude::*;
 use std::{iter, mem::transmute, process::exit};
+mod tile;
+use tile::{*, Tile};
 
-const TILE_TEXTURE_DIMENSION: f32 = 32.0;
-const TILE_TEXTURE_SCALE: f32 = 2.0;
-const TILE_SIZE_PX: f32 = TILE_TEXTURE_DIMENSION * TILE_TEXTURE_SCALE;
-const RIGHT_ANGLE: f32 = -3.141292 / 2.0;
+pub const TILE_TEXTURE_DIMENSION: f32 = 32.0;
+pub const TILE_TEXTURE_SCALE: f32 = 2.0;
+pub const TILE_SIZE_PX: f32 = TILE_TEXTURE_DIMENSION * TILE_TEXTURE_SCALE;
+pub const RIGHT_ANGLE: f32 = -3.141292 / 2.0;
 
 fn main() {
-    let mut level = Vec::from([TileType::Empty; 3]);
-    level.append(&mut Vec::from([TileType::PlayerRot(Side::Left); 1]));
-    level.append(&mut Vec::from([TileType::Empty; 3]));
-    level.append(&mut Vec::from([TileType::Wall; 1]));
-    level.append(&mut Vec::from([TileType::Empty; 3]));
-    level.append(&mut Vec::from([TileType::PlayerRot(Side::Right); 1]));
-    level.append(&mut Vec::from([TileType::RowShift(Facing::Left); 1]));
-    level.append(&mut Vec::from([TileType::Empty; 1]));
-    level.append(&mut Vec::from([TileType::PlayerRot(Side::Right); 1]));
-    level.append(&mut Vec::from([TileType::PlayerRot(Side::Left); 1]));
+    let mut level: Vec<TileStored>;
 
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .insert_resource(Level {
-            width: 4,
-            height: 4,
-            level,
-        })
-        .add_systems(Startup, setup)
-        .add_systems(Update, handle_kb_input)
-        .add_systems(PostUpdate, (render_positioned, render_level))
-        .run();
+    level = [TileEmpty; 8].iter().map(|t| t.wrap()).collect();
+    level.push(Box::new(TileRowShift{facing: Facing::Up}));
+    level.append(&mut [TileEmpty; 3].iter().map(|t| t.wrap()).collect());
+    level.push(Box::new(TilePlayerRot{side: Side::Right}));
+    level.append(&mut [TileEmpty; 3].iter().map(|t| t.wrap()).collect());
+
+    bevy_backend::run(level);
 }
 
-fn render_positioned(
-    mut q: Query<(&mut Transform, &LevelTransform)>,
-    w: Query<&Window, With<PrimaryWindow>>,
-    level: Res<Level>,
-) {
-    let window = w.single();
-
-    for (
-        mut t,
-        LevelTransform {
-            position: p,
-            rotation: rot,
-        },
-    ) in q.iter_mut()
-    {
-        *t = Transform {
-            translation: level_to_world_pos(p.x as u8, p.y as u8, &level, window).extend(0.0),
-            rotation: rot.unwrap_or(Facing::Up).rotation_quat(),
-            ..*t
-        };
-    }
-}
-
-fn render_level(
-    mut q: Query<(&mut Handle<Image>, &mut Tile, &LevelTransform)>,
-    assets: Res<AssetServer>,
-    level: Res<Level>,
-) {
-    for (mut tex, mut tile, &LevelTransform { position: p, .. }) in q.iter_mut() {
-        tile.0 = level.tile_at_vec(p);
-        *tex = assets.load(format!("sprites/tiles/{}.png", tile.0.name()));
-    }
-}
-
-fn handle_kb_input(
-    mut q: Query<&mut LevelTransform, With<Player>>,
-    keyboard_input: Res<Input<KeyCode>>,
-    level: ResMut<Level>,
-) {
-    let level = level.into_inner();
-    let pos = q.single_mut();
-    let transform @ &mut LevelTransform {
-        position: pos,
-        rotation: facing,
-    } = pos.into_inner();
-    match match keyboard_input.get_just_pressed().nth(0) {
-        Some(KeyCode::Up) => Some(1),
-        Some(KeyCode::W) => Some(1),
-        Some(KeyCode::Down) => Some(-1),
-        Some(KeyCode::S) => Some(-1),
-        _ => None,
-    } {
-        Some(n) => {
-            let new_pos: UVec2 =
-                unsafe { transmute(pos.as_ivec2() + n * facing.unwrap_or(Facing::Up).forward()) };
-            let mut rows = level.rows();
-            if new_pos.x < level.width as u32 && new_pos.y < level.height as u32 {
-                let old_pos = transform.position;
-                transform.position = new_pos;
-                match level.tile_at_vec(new_pos).step(level, transform) {
-                    MoveOutcome::OK(o) => transform.position = o.unwrap_or(new_pos),
-                    MoveOutcome::Win => {
-                        info!("Win!");
-                        exit(0)
-                    }
-                    MoveOutcome::Illegal => transform.position = old_pos,
-                }
-                //level.level = rows.iter().flatten().map(|i| *i).collect();
-            }
-        info!("{}", pos.y);
-        }
-        None => {}
+fn move_player(level: &mut Level, player: &mut Position, dir: Facing){
+    let new_pos: UVec2 = unsafe {
+        let rot = player.rotation.unwrap_or(Facing::Up);
+        let increment = rot.add_rotation(dir).forward();
+        transmute(player.position.as_ivec2() + increment) 
     };
-}
-
-fn level_to_world_pos(x: u8, y: u8, level: &Level, window: &Window) -> Vec2 {
-    let parity_offset = |n: u8| if n % 2 == 0 { 0.0 } else { -0.5 };
-    Vec2::new(
-        (window.width() / 2.0)
-            + ((x as i8 - level.width as i8 / 2) as f32 + parity_offset(level.width as u8))
-                * TILE_SIZE_PX,
-        (window.height() / 2.0)
-            + ((y as i8 - level.height as i8 / 2) as f32 + parity_offset(level.height as u8))
-                * TILE_SIZE_PX,
-    )
-}
-
-fn setup(
-    mut commands: Commands,
-    q: Query<&Window, With<PrimaryWindow>>,
-    assets: Res<AssetServer>,
-    level: Res<Level>,
-) {
-    let window = q.get_single().expect("Only one primary window");
-
-    commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 1.0),
-        ..default()
-    });
-
-    for i in 0..level.width {
-        for j in 0..level.height {
-            let (i, j) = (i as u8, j as u8);
-            commands.spawn((
-                SpriteBundle {
-                    transform: Transform::from_scale(Vec3::splat(TILE_TEXTURE_SCALE)),
-                    texture: assets
-                        .load(format!("sprites/tiles/{}.png", level.tile_at(i, j).name())),
-                    ..default()
-                },
-                LevelTransform::pos(i, j),
-                Tile(level.tile_at(i, j)),
-            ));
+    if new_pos.x < level.width as u32 && new_pos.y < level.height as u32 {
+        let old_pos = player.position;
+        player.position = new_pos;
+        match level.tile_at_vec(new_pos).step(level, player) {
+            MoveOutcome::OK(o, _) => player.position = o.unwrap_or(new_pos),
+            MoveOutcome::Win => {
+                info!("Win!");
+                exit(0)
+            }
+            MoveOutcome::Illegal => player.position = old_pos,
         }
     }
 
-    commands.spawn((
-        SpriteBundle {
-            transform: Transform::from_scale(Vec3::splat(TILE_TEXTURE_SCALE)),
-            texture: assets.load("sprites/player.png"),
-            ..default()
-        },
-        LevelTransform::new(0, 0, Some(Facing::Up)),
-        Player,
-    ));
 }
 
 #[derive(Resource)]
 struct Level {
     width: u8,
     height: u8,
-    level: Vec<TileType>,
+    level: Vec<TileStored>,
+    side_moves: u8
 }
 
 impl Level {
-    fn tile_at(&self, x: u8, y: u8) -> TileType {
-        self.level[(y * self.width as u8 + x) as usize]
+    fn tile_at(&self, x: u8, y: u8) -> TileStored {
+        self.level[(y * self.width as u8 + x) as usize].clone()
     }
 
-    fn tile_at_vec(&self, p: UVec2) -> TileType {
+    fn tile_at_vec(&self, p: UVec2) -> TileStored {
         self.tile_at(p.x as u8, p.y as u8)
     }
 
-    fn rows(&self) -> Vec<Vec<TileType>> {
-        let mut result: Vec<Vec<TileType>> = iter::repeat(
-            iter::repeat(TileType::Empty)
+    fn rows(&self) -> Vec<Vec<TileStored>> {
+        let mut result: Vec<Vec<TileStored>> = iter::repeat(
+            iter::repeat(TileEmpty)
                 .take(self.width as usize)
-                .collect(),
+                .map(|t| Box::new(t) as TileStored)
+                .collect()
         )
         .take(self.height as usize)
         .collect();
@@ -191,104 +77,46 @@ impl Level {
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
-enum TileType {
-    Empty,
-    Wall,
-    Finish,
-    PlayerRot(Side),
-    RowShift(Facing),
+#[derive(Component, Clone, Copy)]
+struct Position {
+    position: UVec2,
+    rotation: Option<Facing>,
+}
+impl Position {
+    fn pos(x: u8, y: u8) -> Self {
+        Self {
+            position: UVec2::new(x as u32, y as u32),
+            rotation: None,
+        }
+    }
+
+    fn pos_vec(position: UVec2) -> Self {
+        Self {
+            position,
+            rotation: None,
+        }
+    }
+
+    fn new(x: u8, y: u8, rotation: Option<Facing>) -> Self {
+        Self {
+            position: UVec2::new(x as u32, y as u32),
+            rotation,
+        }
+    }
+
+    fn new_vec(position: UVec2, rotation: Option<Facing>) -> Self {
+        Self { position, rotation }
+    }
 }
 
-impl TileType {
-    fn name(&self) -> String {
-        use TileType::*;
-        match self {
-            Empty => String::from("empty"),
-            Wall => String::from("wall"),
-            Finish => String::from("finish"),
-            PlayerRot(side) => format!(
-                "player_rot_{}",
-                match side {
-                    Side::Right => "right",
-                    Side::Left => "left",
-                }
-            ),
-            RowShift(facing) => format!(
-                "row_shift_{}",
-                match facing {
-                    Facing::Up => "up",
-                    Facing::Right => "right",
-                    Facing::Down => "down",
-                    Facing::Left => "left",
-                }
-            ),
-        }
-    }
-    fn step(&self, level: &mut Level, player: &mut LevelTransform) -> MoveOutcome {
-        use TileType::*;
-        match self {
-            Empty => MoveOutcome::OK(None),
-            Wall => MoveOutcome::Illegal,
-            Finish => MoveOutcome::Win,
-            PlayerRot(side) => {
-                player.rotation =
-                    Some(player.rotation.unwrap_or(Facing::Up).rotate_by(match side {
-                        Side::Left => -1,
-                        Side::Right => 1,
-                    }));
-                MoveOutcome::OK(None)
-            }
-            RowShift(facing) => {
-                let mut new_pos = player.position;
-                let mut shift_horizontal = |dir: Facing| {
-                    info!("idx {}, y {}", player.position.y, player.position.y);
-                    let mut rows = level.rows();
-                    let mut row = rows[player.position.y as usize].clone();
-                    match dir {
-                        Facing::Right => {
-                            row.insert(0, row[row.len() - 1]);
-                            row.remove(row.len() - 1);
-                        }
+#[derive(Component)]
+struct Player;
 
-                        Facing::Left => {
-                            row.push(row[0]);
-                            row.remove(0);
-                        }
-                        _ => panic!("Not a horiz shift"),
-                    };
-                    rows.remove(player.position.y as usize);
-                    rows.insert(player.position.y as usize, row);
-                    level.level = rows.iter().flatten().map(|t| *t).collect();
-                    new_pos.x = ((player.position.x as i32 + dir.forward().x + level.width as i32) % level.width as i32) as u32;
-                };
-
-                match facing {
-                    Facing::Right => shift_horizontal(Facing::Right),
-                    Facing::Left => shift_horizontal(Facing::Left),
-                    _ => {
-                        let offset = match facing {
-                            Facing::Up => 1,
-                            Facing::Down => -1,
-                            _ => panic!("Not a vert shift"),
-                        };
-                        let old_level = level.rows().clone();
-                        let mut old_level_mut = old_level.clone();
-                        old_level_mut.iter_mut().for_each(|mut row| {
-                            row.insert(
-                                player.position.x as usize,
-                                old_level[(player.position.y as isize + offset).abs() as usize]
-                                    [player.position.x as usize],
-                            );
-                            row.remove(player.position.x as usize);
-                        });
-                        level.level = old_level_mut.iter().flatten().map(|t| *t).collect()
-                    }
-                };
-                MoveOutcome::OK(Some(new_pos))
-            }
-        }
-    }
+#[derive(Component)]
+enum MoveOutcome {
+    OK(Option<UVec2>, bool),
+    Illegal,
+    Win,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -338,30 +166,14 @@ impl Facing {
     }
 
     fn rotate_by(&self, amount: i8) -> Facing {
-        use Facing::*;
-        match (amount % 4).abs() {
-            1 => match self {
-                Up => Right,
-                Right => Down,
-                Down => Left,
-                Left => Up,
-            },
-            2 => match self {
-                Up => Down,
-                Right => Left,
-                Down => Up,
-                Left => Right,
-            },
-            3 => match self {
-                Up => Left,
-                Right => Up,
-                Down => Right,
-                Left => Down,
-            },
-            4 => *self,
-            _ => {
-                panic!("-1<x%4<4");
-            }
+        unsafe{
+            transmute(transmute::<Facing, i8>(*self) + amount)
+        }
+    }
+
+    fn add_rotation(&self, rhs: Facing) -> Facing{
+        unsafe{
+            transmute(transmute::<Facing, i8>(*self) + transmute::<Facing, i8>(rhs))
         }
     }
 
@@ -385,46 +197,14 @@ impl Into<Facing> for Side {
     }
 }
 
-#[derive(Component)]
-struct LevelTransform {
-    position: UVec2,
-    rotation: Option<Facing>,
-}
-impl LevelTransform {
-    fn pos(x: u8, y: u8) -> Self {
-        Self {
-            position: UVec2::new(x as u32, y as u32),
-            rotation: None,
-        }
-    }
-
-    fn pos_vec(position: UVec2) -> Self {
-        Self {
-            position,
-            rotation: None,
-        }
-    }
-
-    fn new(x: u8, y: u8, rotation: Option<Facing>) -> Self {
-        Self {
-            position: UVec2::new(x as u32, y as u32),
-            rotation,
-        }
-    }
-
-    fn new_vec(position: UVec2, rotation: Option<Facing>) -> Self {
-        Self { position, rotation }
-    }
-}
-
-#[derive(Component)]
-struct Player;
-
-#[derive(Component)]
-struct Tile(TileType);
-
-enum MoveOutcome {
-    OK(Option<UVec2>),
-    Illegal,
-    Win,
+fn level_to_world_pos(x: u8, y: u8, level: &Level, window: &Window) -> Vec2 {
+    let parity_offset = |n: u8| if n % 2 == 0 { 0.0 } else { -0.5 };
+    Vec2::new(
+        (window.width() / 2.0)
+            + ((x as i8 - level.width as i8 / 2) as f32 + parity_offset(level.width as u8))
+                * TILE_SIZE_PX,
+        (window.height() / 2.0)
+            + ((y as i8 - level.height as i8 / 2) as f32 + parity_offset(level.height as u8))
+                * TILE_SIZE_PX,
+    )
 }
